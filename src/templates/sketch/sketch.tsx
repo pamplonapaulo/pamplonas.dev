ca755555dbb0ef1330f8d7c4f72a60bb01bed6a5
pamplonapaulo <25485709+pamplonapaulo@users.noreply.github.com>
2022-09-06 21:41:02 -0300

    vercel error

--- src/templates/sketch/sketch.tsx
+++ src/templates/sketch/sketch.tsx
@@ -1,16 +1,11 @@
 import Container from 'components/container'
 import Sketch from 'components/sketch'
-import * as S from './styles'
 
 const SketchTemplate = () => {
   return (
-    <>
-      {/* <S.Main> */}
-      <Container justify={'space-around'} maxWidth={'1480px'}>
-        <Sketch />
-      </Container>
-      {/* </S.Main> */}
-    </>
+    <Container justify={'space-around'} maxWidth={'1480px'}>
+      <Sketch />
+    </Container>
   )
 }
 
--- src/templates/sketch/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Main = styled.main`
-  background-image: linear-gradient(#000, #1b1b1b);
-  min-height: calc(100vh - 83px);
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`
