3dd8b7cda5115419e2fe6a55d72320b6541cdbb2
pamplonapaulo <25485709+pamplonapaulo@users.noreply.github.com>
2022-09-29 00:11:20 -0300

    font face flickering fixed

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import * as S from './styles'
 
@@ -6,58 +7,61 @@ import { useMenu } from 'contexts'
 const Nav = () => {
   const { menu, setMenu } = useMenu()
   const router = useRouter()
+  const [current, setCurrent] = useState('')
 
   const handleClick = (path: string) => {
     router.push({ pathname: path }, path)
     setMenu(false)
+    setCurrent(path)
   }
+
+  useEffect(() => {
+    setCurrent(window.location.pathname)
+  }, [])
 
   return (
     <S.Nav isVisible={menu}>
-      <S.Item
-        isCurrent={window.location.pathname === '/'}
-        onClick={() => handleClick('/')}
-      >
+      <S.Item isCurrent={current === '/'} onClick={() => handleClick('/')}>
         Home
       </S.Item>
       <S.Item
-        isCurrent={window.location.pathname === '/about'}
+        isCurrent={current === '/about'}
         onClick={() => handleClick('/about')}
       >
         About
       </S.Item>
       <S.Item
-        isCurrent={window.location.pathname === '/portfolio'}
+        isCurrent={current === '/portfolio'}
         onClick={() => handleClick('/portfolio')}
       >
         Portfolio
       </S.Item>
       <S.Item
-        isCurrent={window.location.pathname === '/stack'}
+        isCurrent={current === '/stack'}
         onClick={() => handleClick('/stack')}
       >
         Stack
       </S.Item>
       <S.Item
-        isCurrent={window.location.pathname === '/resume'}
+        isCurrent={current === '/resume'}
         onClick={() => handleClick('/resume')}
       >
         Resume
       </S.Item>
       <S.Item
-        isCurrent={window.location.pathname === '/articles'}
+        isCurrent={current === '/articles'}
         onClick={() => handleClick('/articles')}
       >
         Articles
       </S.Item>
       <S.Item
-        isCurrent={window.location.pathname === '/generative'}
+        isCurrent={current === '/generative'}
         onClick={() => handleClick('/generative')}
       >
         Generative Art
       </S.Item>
       <S.Item
-        isCurrent={window.location.pathname === '/contact'}
+        isCurrent={current === '/contact'}
         onClick={() => handleClick('/contact')}
       >
         Contact
