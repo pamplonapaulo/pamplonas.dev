d53ba967f5384f727a86b2730bec5999ba145d7b
pamplonapaulo <25485709+pamplonapaulo@users.noreply.github.com>
2022-11-10 18:10:13 -0300

    what a messa I did

--- src/arts/clippingMask/clippingMask.tsx
+++ src/arts/clippingMask/clippingMask.tsx
@@ -1,10 +1,10 @@
 import React, { useMemo, useEffect, useState, useCallback, useRef } from 'react'
 import * as S from './styles'
 import { createRandom } from 'utils/random'
-import { randomRange, colorRandom, degToRad, mapRange } from 'utils/canvas'
+import { randomRange, colorRandom, degToRad } from 'utils/canvas'
 import { useCanvas } from 'contexts'
-
-// type CanvasLineCap = 'butt' | 'round' | 'square'
+import risoColors from 'riso-colors'
+import Color from 'canvas-sketch-util/color'
 
 const ClippingMask = () => {
   let canvas = useRef<HTMLCanvasElement | null>(null)
@@ -15,24 +15,20 @@ const ClippingMask = () => {
     h: 0,
   })
   const { setCanvas } = useCanvas()
-  const [color, setColor] = useState('')
   const [animateOn, setAnimateOn] = useState(false)
+  const { pick, value } = createRandom()
 
   let anime = useRef<number>(0)
+  const num = 40
+  const degrees = -30
+
+  const rects = useMemo(() => [], [])
 
   const drawSkewdRect = useCallback(
-    (
-      current: CanvasRenderingContext2D,
-      color: string,
-      w = 600,
-      h = 200,
-      deg = -45
-    ) => {
+    (current: CanvasRenderingContext2D, w = 600, h = 200, deg = -45) => {
       const angle = degToRad(deg)
       const rx = Math.cos(angle) * w
       const ry = Math.sin(angle) * w
-
-      current.strokeStyle = color
 
       current.save()
       current.translate(rx * -0.5, (ry + h) * -0.5)
@@ -53,29 +49,73 @@ const ClippingMask = () => {
     (frameCounter?: number) => {
       if (ctx.current === null) return
 
-      const num = 20
-      const degrees = -30
-
-      let x: number, y: number, w: number, h: number, rx: number, ry: number
-
-      for (let i = 0; i < num; i++) {
-        x = randomRange(0, dimensions.w)
-        y = randomRange(0, dimensions.h)
-        w = randomRange(200, 600)
-        h = randomRange(40, 200)
+      rects.forEach((rect) => {
+        const { x, y, w, h, fill, stroke, blend } = rect
 
         ctx.current.save()
         ctx.current.translate(x, y)
+        ctx.current.strokeStyle = stroke
+        ctx.current.fillStyle = fill
+        ctx.current.lineWidth = 10
 
-        drawSkewdRect(ctx.current, color, w, h, degrees)
+        ctx.current.globalCompositeOperation = blend
+
+        drawSkewdRect(ctx.current, w, h, degrees)
+
+        let shadowColor = Color.offsetHSL(fill, 0, 0, -20)
+        shadowColor.rgba[3] = 0.5
+
+        ctx.current.shadowColor = Color.style(shadowColor.rgba)
+        ctx.current.shadowOffsetX = -10
+        ctx.current.shadowOffsetY = 20
+
+        ctx.current.fill()
+
+        ctx.current.shadowColor = null
         ctx.current.stroke()
+
+        ctx.current.globalCompositeOperation = 'source-over'
+
+        ctx.current.lineWidth = 2
+        ctx.current.strokeStyle = 'black'
+        ctx.current.stroke()
+
         ctx.current.restore()
-      }
+      })
 
       setCanvas(canvas.current)
     },
-    [color, setCanvas, dimensions, drawSkewdRect]
+    [setCanvas, drawSkewdRect, degrees, rects]
   )
+
+  const defineRects = useCallback(() => {
+    const rectColors = [pick(risoColors).hex, pick(risoColors).hex]
+
+    for (let i = 0; i < num; i++) {
+      let x: number,
+        y: number,
+        w: number,
+        h: number,
+        fill: string,
+        stroke: string,
+        blend: string
+
+      x = randomRange(0, dimensions.w)
+      y = randomRange(0, dimensions.h)
+      w = randomRange(600, dimensions.w)
+      h = randomRange(40, 200)
+      // w = randomRange(200, 600)
+      // h = randomRange(40, 200)
+
+      fill = colorRandom(rectColors)
+      stroke = colorRandom(rectColors)
+
+      blend = value() > 0.5 ? 'overlay' : 'source-over'
+
+      rects.push({ x, y, w, h, fill, stroke, blend })
+    }
+    render()
+  }, [dimensions, rects, render, pick, value])
 
   useEffect(() => {
     const h =
@@ -88,15 +128,6 @@ const ClippingMask = () => {
       w,
       h,
     })
-
-    const color = colorRandom([
-      '#3494DF',
-      '#38C1AD',
-      '#60079F',
-      '#F32C43',
-      '#f8f32b',
-    ])
-    setColor(color)
   }, [])
 
   const animate = useCallback(() => {
@@ -117,13 +148,13 @@ const ClippingMask = () => {
       canvas.current.width = dimensions.w
       ctx.current = canvas.current.getContext('2d')
 
-      // cover background with black
-      ctx.current.fillStyle = '#000'
+      // cover canvas background with black
+      //ctx.current.fillStyle = '#000'
+      ctx.current.fillStyle = pick(risoColors).hex
       ctx.current.fillRect(0, 0, dimensions.w, dimensions.h)
-
-      render()
+      defineRects()
     }
-  }, [dimensions, render])
+  }, [dimensions, render, defineRects, pick])
 
   return <S.Canvas ref={canvas} />
 }
